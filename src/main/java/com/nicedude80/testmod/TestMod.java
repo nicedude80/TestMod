d356ca2ac3f7074d7b3ac646ed2d228ba38b23d2
nicedude80 <smurfman1234@hotmail.com>
2015-07-12 13:27:53 -0600

    Added TestMod.java (main class)

---
+++ src/main/java/com/nicedude80/testmod/TestMod.java
@@ -0,0 +1,20 @@
+package com.nicedude80.testmod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = TestMod.MODID, version = TestMod.VERSION)
+public class TestMod
+{
+    public static final String MODID = "LotheransHallTestMod";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
